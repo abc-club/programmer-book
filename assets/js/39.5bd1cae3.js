a03c18796118e2f73fdfe6b105cb82a6ec22cd97
zoro <aoping@yougola.com>
2020-03-23 15:27:24 +0800

    deploy

--- assets/js/39.5bd1cae3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{230:function(t,e,n){"use strict";n.r(e);var s=n(0),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("苹果每年6月开WWDC")])])}),[],!1,null,null,null);e.default=l.exports}}]);
